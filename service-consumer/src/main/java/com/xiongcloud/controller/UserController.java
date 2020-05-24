4c91d63f8c073ecd3a7c5113fc805ac7e847d61a
xiongxikai <xx64@njit.edu>
2020-05-24 00:04:00 -0400

    define global fallback method for hystrix service downgrade

--- service-consumer/src/main/java/com/xiongcloud/controller/UserController.java
+++ service-consumer/src/main/java/com/xiongcloud/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.xiongcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("consumer/user")
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class UserController {
 
     @Autowired
@@ -18,14 +20,13 @@ public class UserController {
 
 
     @GetMapping("{id}")
-    @HystrixCommand(fallbackMethod = "queryUserByIdFallback")
+    @HystrixCommand
     public String queryUserById(@PathVariable("id") Long id) {
         return this.restTemplate.getForObject("http://service-provider/user/" + id, String.class);
     }
 
-    //broker method return type must be the same as the method to be broken
-    public String queryUserByIdFallback(Long id) {
-        return "busy server, try later";
+    public String defaultFallback() {
+        return "busy server, try later, default method";
     }
 
 }
